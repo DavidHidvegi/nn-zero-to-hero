f7a602ae47ebf5293df2eaa4f7cef86ca3e8114b
David Hidvegi <hidvegidave@gmail.com>
2023-03-03 10:00:08 +0100

    lec7: Transformer block without residual connection, loss: 2.39

--- lectures/lecture7_gpt/gpt.py
+++ lectures/lecture7_gpt/gpt.py
@@ -103,6 +103,21 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         return self.net(x)
+    
+class Block(nn.Module):
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd lile
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        x = self.sa(x)
+        x = self.ffwd(x)
+        return x
+
 
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
@@ -112,8 +127,12 @@ class BigramLanguageModel(nn.Module):
         
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_heads = MultiHeadAttention(4, n_embd//4) # i.e. 4 heads of 8-dimensional self-attention
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+        )
+
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -123,8 +142,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C)
-        x = self.sa_heads(x) # apply one head of self-attention (B, T, H)
-        x = self.ffwd(x) # (B, T, C)
+        x = self.blocks(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
